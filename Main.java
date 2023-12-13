ea3c014247d230644353f824f813dd48ed67a0bf
khalid00103 <78197331+khalid00103@users.noreply.github.com>
2023-12-13 16:16:24 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,6 @@
+class Main{
+    public static void main(String args[]){
+        System.out.println("Hello this Khalid Shaikh");
+        System.out.println("This is my Java Program to make a Docker Image");
+    }
+}
